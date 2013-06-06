95098f5db0c8f9e5a28981e2cb04522b6d5fa9c4
Koen <k.klinkers@gmail.com>
2013-06-06 17:16:12 +0200

    sounds and paradise class

---
+++ Paradise.py
@@ -0,0 +1,37 @@
+# kloki
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Koen Klinkers k.klinkers@gmail.com
+
+class Paradise(object):
+
+    def __init__(self,name,song,text):
+        self.name=name+(" "*(40-len(name)))
+        self.song=song
+        self.text=text
+
+    def __str__(self):
+        pass
+
+
+    # this way == and != work with this object
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
